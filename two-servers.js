b9ade601deb350205a4385720eed34c8ec3a440e
Brandon <bjames9076@gmail.com>
2018-01-16 10:53:26 -0500

    added extra port

---
+++ two-servers.js
@@ -0,0 +1,24 @@
+// Require/import the HTTP module
+var http = require("http");
+
+// Define a port to listen for incoming requests
+var PORT_01 = 7000;
+
+var PORT_02 = 7500;
+// Create a generic function to handle requests and responses
+function handleRequest_01(request, response) {
+
+
+  response.end("It Works!! Path Hit: " + request.url);
+}
+
+var server_01 = http.createServer(handleRequest_01);
+
+var server_02 = http.createServer(handleRequest_01);
+
+// Start our server so that it can begin listening to client requests.
+server.listen(PORT, function() {
+
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
